19b21767dfa71cfb7020f877342c872bd555ce54
Chronicle20 <a.chronicle.20@gmail.com>
2025-02-09 18:27:42 -0500

    Add .bruno examples. Fix deserialization.

--- atlas.com/skills/skill/rest.go
+++ atlas.com/skills/skill/rest.go
@@ -1,12 +1,32 @@
 package skill
 
-import "time"
+import (
+	"strconv"
+	"time"
+)
 
 type RestModel struct {
 	Id          uint32    `json:"-"`
 	Level       byte      `json:"level"`
 	MasterLevel byte      `json:"masterLevel"`
 	Expiration  time.Time `json:"expiration"`
+}
+
+func (r RestModel) GetName() string {
+	return "skills"
+}
+
+func (r RestModel) GetID() string {
+	return strconv.Itoa(int(r.Id))
+}
+
+func (r *RestModel) SetID(strId string) error {
+	id, err := strconv.Atoi(strId)
+	if err != nil {
+		return err
+	}
+	r.Id = uint32(id)
+	return nil
 }
 
 func Transform(m Model) (RestModel, error) {
